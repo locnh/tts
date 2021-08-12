638d55f4f79f1166e00f6176e9593129e3c46c20
Loc Nguyen <me@locnh.com>
2021-08-12 12:31:52 +0200

    Add HTML sample code (#3)
    * Add try-it page
    * Add html sample code

--- main.go
+++ main.go
@@ -150,9 +150,10 @@ func getRawAudioLink(payload string) string {
 
 func returnRaw(raw bool) func(c *gin.Context) {
 	return func(c *gin.Context) {
-		filename := processData(c)
-
-		if _, err := os.Stat(storage_path + "/" + filename + ".mp3"); os.IsNotExist(err) {
+		if filename := processData(c); filename == "" {
+			c.JSON(http.StatusBadRequest,
+				gin.H{"error": "Payload empty"})
+		} else if _, err := os.Stat(storage_path + "/" + filename + ".mp3"); os.IsNotExist(err) {
 			c.JSON(http.StatusInternalServerError,
 				gin.H{"error": "Server Internal Error"})
 		} else {
@@ -167,9 +168,10 @@ func returnRaw(raw bool) func(c *gin.Context) {
 }
 
 func returnJSON(c *gin.Context) {
-	filename := processData(c)
-
-	if _, err := os.Stat(storage_path + "/" + filename + ".mp3"); os.IsNotExist(err) {
+	if filename := processData(c); filename == "" {
+		c.JSON(http.StatusBadRequest,
+			gin.H{"error": "Payload empty"})
+	} else if _, err := os.Stat(storage_path + "/" + filename + ".mp3"); os.IsNotExist(err) {
 		c.JSON(http.StatusInternalServerError,
 			gin.H{"error": "Server Internal Error"})
 	} else {
@@ -180,6 +182,10 @@ func returnJSON(c *gin.Context) {
 
 func processData(c *gin.Context) string {
 	content, _ := ioutil.ReadAll(c.Request.Body)
+
+	if len(content) == 0 {
+		return ""
+	}
 
 	sha1HashInBytes := sha1.Sum(content)
 	filename := hex.EncodeToString(sha1HashInBytes[:])
